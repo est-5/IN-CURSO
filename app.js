3b359c5e8a717c347038904e4b71cf328dc895e3
est-5 <estelar.5000xd@gmail.com>
2026-07-20 17:54:21 -0600

    Add button click event to redirect to dashboard

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("Bienvenido a IN-CURSO");
+const boton=document.getElementById("continuar");
+
+boton.onclick=function(){
+
+localStorage.setItem("videoVisto","si");
+
+window.location.href="dashboard.html";
+
+}
